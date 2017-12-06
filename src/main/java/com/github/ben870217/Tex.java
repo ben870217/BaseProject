675e98151db9afa109268ab7a3aa7d81acfafa94
ben <ben870217@yahoo.com.tw>
2017-12-06 12:04:32 +0800

    succeed

---
+++ src/main/java/com/github/ben870217/Tex.java
@@ -0,0 +1,13 @@
+package com.github.ben870217;
+
+public class Tex {
+    public  int aa(int a){
+        a=a*a;
+        if(a>0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/com/github/ben870217/TexTest.java
@@ -0,0 +1,16 @@
+package com.github.ben870217;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TexTest {
+    @Test
+    public void aa() throws Exception {
+        Tex s=new Tex();
+        int c=s.aa(5);
+        Assert.assertArrayEquals(1,c);
+    }
+
+}
